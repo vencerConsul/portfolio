4191bafcbc9717526b41d3d58349794535cebd42
vencerConsul <o.lermovenz@gmail.com>
2023-07-14 13:13:43 +0800

    small changes

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default () =>{
     return axios.create({
-        baseURL: 'https://vencer-olermo.vercel.app'
+        baseURL: 'https://vencer-olermo.vercel.app:9000'
     });
 }
