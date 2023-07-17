fe8694417c1a012d97aaeb23a320a5729213abfb
vencerConsul <o.lermovenz@gmail.com>
2023-07-18 02:21:28 +0800

    small changes

--- src/components/HighlightedProjects.vue
+++ src/components/HighlightedProjects.vue
@@ -47,8 +47,8 @@ export default {
                     thumbnail: '/timesheet.png',
                     title: 'Time-sheet',
                     paragraph: 'Is a Laravel web app that track the employee time in and time out (Break) in real time using websocket, I made this project to my previous company.',
-                    buttonText: 'Unavailble',
-                    buttonUrl: 'https://github.com/vencerConsul/management',
+                    buttonText: 'Take a look',
+                    buttonUrl: 'https://timesheet.up.railway.app/',
                     technologiesImg: ['/icons/laravel.png', '/icons/javascript.png', '/icons/mysql.png', '/icons/php.png']
                 },
                 {
